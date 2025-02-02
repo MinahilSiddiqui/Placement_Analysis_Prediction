742227771940aa429d4cedc13034fb7edb685f50
MinahilSiddiqui <manahalsiddiqui@gmail.com>
2025-02-02 18:20:52 +0500

    requirements

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
